c81e577db63f6e258429bb6b2dc5c485ac5a44a1
JDLO <juandavidleono@gmail.com>
2026-02-11 13:45:03 +0100

    initial of the project

---
+++ src/main/java/com/bezkoder/spring/restapi/model/Tutorial.java
@@ -0,0 +1,78 @@
+package com.bezkoder.spring.restapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tutorial")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tutorial {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id = 0;
+
+	@Column(name = "title")
+	private String title;
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "published")
+	private boolean published;
+	
+	public Tutorial() {}
+
+	public Tutorial(String title, String description, boolean published) {
+		this.title = title;
+		this.description = description;
+		this.published = published;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isPublished() {
+		return published;
+	}
+
+	public void setPublished(boolean isPublished) {
+		this.published = isPublished;
+	}
+
+	@Override
+	public String toString() {
+		return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+	}
+
+}
